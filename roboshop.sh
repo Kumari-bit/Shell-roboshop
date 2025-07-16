06e8159ee12b8d16045b71cf3327880878b258e3
Kumari <kumarivenkata994@gmail.com>
2025-07-16 16:50:48 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ ZONE_ID="Z089235936D5DMGV67BYD" # replace with your ZONE ID
 DOMAIN_NAME="daws85s.cyou" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
-for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-091d30e70a2b591df --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
@@ -15,6 +15,8 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+
     fi
     echo "$instance IP address: $IP"
 done
