a646c2e7ea3f289d921ac19fb418e38743095395
Kumari <kumarivenkata994@gmail.com>
2026-01-02 20:07:40 +0530

    sheell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -76,5 +76,13 @@ VALIDATE $? "Starting Catalogue"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
-echo -e "Data is already loaded ... $Y SKIPPING $N"
+
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
+
